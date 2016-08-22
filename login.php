85fde394a78260b950274f4345960f666b3df9b6
Harshvardhan58 <harshvardhan.singh@technonjr.org>
2016-08-22 14:46:48 -0700

    login
    day 1

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Technolx</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css"/>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="top">
+                <div id="heading">
+                    
+                </div>
+               
+            </div>
+            <div id="content">
+                <div id="login_form">
+                </div>
+            </div>
+            <div id="footer">
+                
+            </div>
+        </div>
+    </body>
+</html>
